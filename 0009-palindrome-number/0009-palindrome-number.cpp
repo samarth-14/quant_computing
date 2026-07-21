4b201d244f66a3555eb8dc905d0c145dde6d29ae
samarth-14 <samarthdiwakar0@gmail.com>
2026-07-21 22:52:29 +0530

    Time: 4 ms (31.21%), Space: 10.8 MB (12.77%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0||x%10==0&&x!=0){
-            return false;
-        }
-        int h=0;
-        while(x>h){
-            h=h*10+x%10;
-            x/=10;
-        }
-       return x==h||x==h/10;
+        if(x<0)return false;
+        string s=to_string(x);
+        string r=s;
+        reverse(r.begin(),r.end());
+        return s==r;
     }
 };
