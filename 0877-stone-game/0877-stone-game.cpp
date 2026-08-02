1cd79052b58322deee63421a4d33a67101428402
samarth-14 <samarthdiwakar0@gmail.com>
2026-08-02 17:16:46 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.75%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n =piles.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int i=0;i<n;i++){
-            dp[i][i]=piles[i];
-        }
-        for(int l=2;l<=n;l++){
-            for(int i =0;i<=n-l;i++){
-                int j =i+l-1;
-                dp[i][j]=max(piles[i]-dp[i+1][j],piles[j]-dp[i][j-1]);
-            }
-        }
-        return dp[0][n-1]>0;
+        return true;
     }
 };
